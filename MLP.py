a241c82fafbbb1d4829f50b392466430882dc090
IvanHuarte <ivanhuarte@hotmail.com>
2025-05-13 22:21:58 +0200

    Developing

--- MLP.py
+++ MLP.py
@@ -1,79 +1,44 @@
 import numpy as np
 import jax
 import jax.numpy as jnp
-import flax
 import flax.linen as nn
-import optax
 import netket as nk
-from typing import Tuple, Callable, Any, Dict, Optional
-import numpy.typing as npt
-import copy
-import pathlib
-import matplotlib.pyplot as plt
+from typing import Tuple, Callable, Any
+import json
 import time
+import os
 
 from VA_project.model.model import OxalateJKGamma
 from VA_project.engine.runners import Runner
-from NN_utils import dump_callback, save_results
+from NN_utils import dump_callback, save_results, BestIterKeeper
 
 jax.config.update("jax_enable_x64", True)
 jax.config.update("jax_platform_name", "gpu")
 jax.devices()
 
+act_dict={
+    'sigmoid': nn.sigmoid,
+    'tanh': nn.tanh,
+    'softmax': nn.softmax,
+    'gelu': nn.gelu,
+    'swish': nn.swish,
+    'selu': nn.selu,
+    'elu': nn.elu,
+    'softplus': nn.softplus,
+    'relu': nn.relu
+}
 
-class BestIterKeeper:
-    """Store the values of a bunch of quantities from the best iteration.
+opt_name_dict={
+    'Sgd': nk.optimizer.Sgd,
+    'adam': nk.optimizer.Adam,
+    'AdaGrad': nk.optimizer.AdaGrad
+}
 
-    "Best" is defined in the sense of lowest energy.
-
-    Args:
-        Hamiltonian: An array containing the Hamiltonian matrix.
-        N: The number of spins in the chain.
-        baseline: A lower bound for the V score. If the V score of the best
-            iteration falls under this threshold, the process will be stopped
-            early.
-        filename: Either None or a file to write the best state to.
-    """
-
-    def __init__(
-        self,
-        Hamiltonian: npt.ArrayLike,
-        N: int,
-        baseline: float,
-        filename: Optional[pathlib.Path] = None,
-    ):
-        self.Hamiltonian = Hamiltonian
-        self.N = N
-        self.baseline = baseline
-        self.filename = filename
-        self.vscore = np.inf
-        self.best_energy = np.inf
-        self.best_state = None
-
-    def update(self, step, log_data, driver):
-        """Update the stored quantities if necessary.
-
-        This function is intended to act as a callback for NetKet. Please refer
-        to its API documentation for a detailed explanation.
-        """
-        vstate = driver.state
-        energystep = np.real(vstate.expect(self.Hamiltonian).mean)
-        var = np.real(getattr(log_data[driver._loss_name], "variance"))
-        mean = np.real(getattr(log_data[driver._loss_name], "mean"))
-        varstep = self.N * var / mean**2
-
-        if self.best_energy > energystep:
-            self.best_energy = energystep
-            self.best_state = copy.copy(driver.state)
-            self.best_state.parameters = flax.core.copy(driver.state.parameters)
-            self.vscore = varstep
-
-            if self.filename != None:
-                with open(self.filename, "wb") as file:
-                    file.write(flax.serialization.to_bytes(driver.state))
-
-        return self.vscore > self.baseline
-    
+sampler_dict={
+    'MetropolisLocal': nk.sampler.MetropolisLocal,
+    'MetropolisExchange': nk.sampler.MetropolisExchange,
+    'MetropolisHamiltonian': nk.sampler.MetropolisHamiltonian
+}
 
 class MLP(nn.Module):
     """A simple multi-layer perceptron."""
@@ -103,206 +68,188 @@ class MLP(nn.Module):
         return x.squeeze(-1)
     
 
-settings={
-    
-    'size': (4,4), #Lattice size
-    'alpha':2.5,
+with open("config.json",'r') as f:
+    config = json.load(f)
 
-    'NN_arch': {
+strength = config['strength']          # Lattice and coupling model
+theta_list = config['theta_list']
+phi_list = config['phi_list']
+sizes = config['sizes']
+kwargs_lattice = config['kwargs_lattice']
 
-        'MLP': {
-
-            'rng' : jax.random.PRNGKey(0),
-            'layers' : ['dense', 'activation', 'dense'],
-            'dimensions' : [128, 128],
-            'activation' : [nn.sigmoid],
-            'dropout' : [],
-
-        }
-    },
-    'optimizer': {
-        'adam': {
-            'learning_rate': 0.01
-        },
-        'Sgd': {
-            'learning_rate': 0.01
-        },
-        'AdaGrad': {
-            'learning_rate': 0.01
-        },
-    },   
-
-    'sampler': {
-        'type': 'MetropolisLocal',
-        'n_samples': 1008,
-        
-    },
-}
-    
-strength = 0.2
-theta = 9.0
-phi = 72.
-
-diag_shift= 0.001
-n_iter=1000
-
-
-act_dict={
-    'sigmoid': nn.sigmoid,
-    'tanh': nn.tanh,
-    'softmax': nn.softmax,
-    'gelu': nn.gelu,
-    'swish': nn.swish,
-    'selu': nn.selu,
-    'elu': nn.elu,
-    'softplus': nn.softplus}
-
-opt_name_dict={
-    'Sgd': nk.optimizer.Sgd,
-    'adam': nk.optimizer.Adam,
-    'AdaGrad': nk.optimizer.AdaGrad} 
-
+dimensions_list = config['dimensions_list']         # MLP architecture settings
+activation_list = config['activation_list']
 opt_name_list = ['Sgd', 'adam', 'AdaGrad']
 learning_rate_list = [0.1, 0.01, 0.001]
 
-dimensions_list = [
-    (32,32,1),
-    (64,64,1),
-    (16,32,64,64,32,16,1),
-    (64,64,32,16,8,4,2,1)
-]
-activation_list = [
-    [('relu',0,0),(0,'relu',0),('softplus',0,0),(0,'softplus',0)],
-    [('relu',0,0),(0,'relu',0),('softplus',0,0),(0,'softplus',0)],
-    [(0,'softmax',0,0,0,0,0),(0,'softmax','softmax',0,0,0,0),(0,'softplus',0,0,0,0,0), (0,'softplus','softplus',0,0,0,0)]
-]
+sampler_name = config['sampler']['name']            # Sampler settings
+n_samples = config['sampler']['n_samples']
+
+iterations = config['iterations']                   # Simulation settings
+exact_diag = config['exact_diagonalization']
+dump_simulation = config['dump_sim_callback']
+write_folder = config['write_folder_sim']
+
+diag_shift= 0.001       # Diagonal shift for the SR preconditioner
+
+
+dimensions_list = [tuple(dim) for dim in dimensions_list]
+activation_list = [[tuple(act) for act in activation] for activation in activation_list]
 
 
 d=0 ; a=0 
+for i, size in enumerate(sizes):
 
-for d, dimensions in enumerate(dimensions_list): 
-    
-    for a, activation_name in enumerate(activation_list[d]):
+    for j, (theta, phi) in enumerate(zip(theta_list, phi_list)):
 
-        activation = tuple([act_dict[act] if act != 0 else 0 for act in activation_name])
+        for d, dimensions in enumerate(dimensions_list): 
+            
+            for a, activation_name in enumerate(activation_list[d]):
 
-        for opt_name in opt_name_list:
+                activation = tuple([act_dict[act] if act != 0 else 0 for act in activation_name])
 
-            for learning_rate in learning_rate_list:
+                for opt_name in opt_name_list:
 
-                print(f"dimensions: {dimensions} \nactivation: {activation_name} \nopt_name: {opt_name} \nlearning_rate: {learning_rate}")
+                    for learning_rate in learning_rate_list:
 
-                time_in = time.time()
-                
-                # Initialize the model
-                model = MLP(
-                    param_dtype=jnp.complex64,
-                    hidden_dims=dimensions,
-                    activation=activation,
-                    )
-                # ...with rng keys 
-                rng= jax.random.PRNGKey(0)
-                key1, key2 = jax.random.split(rng)
-                keys = {'params': key1, 'dropout': key2}
+                        print(f"\n---- Parameters: Size {size}  strength={strength:1f}  theta={theta:2f}  phi={phi:2f} ----\n\n")
+                        print(f"dimensions: {dimensions} \nactivation: {activation_name} \nopt_name: {opt_name} \nlearning_rate: {learning_rate}")
 
-                params = model.init(keys['params'], jnp.ones((4*4), dtype=jnp.complex64)) 
-                output=model.apply(params, jnp.ones((4*4), dtype=jnp.complex64))
-
-                hi = nk.hilbert.Spin(s=0.5, N = int(np.prod(settings['size'])))
-
-                sampler = nk.sampler.MetropolisLocal(hi, dtype=complex)
-                #print(sampler.sample_next(NN_arch, model_params))
-
-                optimizer = opt_name_dict[opt_name](learning_rate=learning_rate)
-
-                vstate = nk.vqs.MCState(sampler, model, n_samples = settings['sampler']['n_samples'])
-                is_holo = nk.utils.is_probably_holomorphic(vstate._apply_fun, vstate.parameters, vstate.samples, vstate.model_state)
-
-                SR= nk.optimizer.SR(diag_shift = diag_shift, holomorphic = True)
-
-                # Create the Hamiltonian
-                oxa=OxalateJKGamma(settings['size'], 
-                                [strength, theta, phi],
-                                    bc='periodic', order='default_2')
-                H = Runner(oxa.cm).build_hamiltonian()
-
-                gs = nk.VMC(
-                    hamiltonian=H,
-                    optimizer=optimizer,
-                    preconditioner=SR,
-                    variational_state=vstate)
-                log = nk.logging.RuntimeLog()
-                keeper= BestIterKeeper(H, np.prod(settings['size']), baseline = 1e-8,
-                    #filename=pathlib.Path("best_state.nk"),
-                )
-                gs.run(n_iter=n_iter, out=log, callback= [keeper.update])
-                time_out = time.time()
-                time_exe= time_out - time_in
-                
-                if ED:
-                    E_ED, x_ED = Runner(oxa.cm).exact_energy_lanczos(eigenstates=True)
-                    E_ED = E_ED.squeeze(-1)
-                    keeper.E_ED = E_ED
-
-
-                sim_label = f"_{d}_{a}_{opt_name}_{learning_rate}"
-                if dump_simulation:
-                    # For plotting architecture
-                    set_dim=[f"D({dim})" for dim in dimensions] 
-                    set_act=[f"A({act})" for act in activation_name]
-                    setup='||'
-                    for i in range(len(dimensions)):
-                        setup+=f" {set_dim[i]} |"
-                        setup+=f" {set_act[i]} |" if '0' not in set_act[i] else ''
+                        time_in = time.time()
                         
-                    setup+="|"
-                    
-                    dump_setup ={'size': settings['size'], 'theta': theta,
-                                'phi': phi, 'opt_name': opt_name,
-                                'learning_rate': learning_rate,
-                                'time_exe': time_exe, 'architecture': setup,
-                                'sim_label': sim_label
-                            }
+                        # Initialize the model
+                        model = MLP(
+                            param_dtype=jnp.complex64,
+                            hidden_dims=dimensions,
+                            activation=activation,
+                            )
+                        # ...with rng keys 
+                        rng= jax.random.PRNGKey(0)
+                        key1, key2 = jax.random.split(rng)
+                        keys = {'params': key1, 'dropout': key2}
+
+                        params = model.init(keys['params'], jnp.ones((4*4), dtype=jnp.complex64)) 
+                        output=model.apply(params, jnp.ones((4*4), dtype=jnp.complex64))
+
+                        hi = nk.hilbert.Spin(s=0.5, N = int(np.prod(size)))
+
+                        sampler = sampler_dict[sampler_name](hi, dtype=complex)
+
+                        optimizer = opt_name_dict[opt_name](learning_rate=learning_rate)
+
+                        vstate = nk.vqs.MCState(sampler, model, n_samples = n_samples)
+                        is_holo = nk.utils.is_probably_holomorphic(vstate._apply_fun, vstate.parameters, vstate.samples, vstate.model_state)
+
+                        SR= nk.optimizer.SR(diag_shift = diag_shift, holomorphic = True)
+
+                        # Create the Hamiltonian
+                        oxa=OxalateJKGamma(
+                            size, 
+                            [strength, theta, phi],
+                            **kwargs_lattice
+                        )
+                        H = Runner(oxa.cm).build_hamiltonian()
+
+                        gs = nk.VMC(
+                            hamiltonian=H,
+                            optimizer=optimizer,
+                            preconditioner=SR,
+                            variational_state=vstate)
+                        
+                        log = nk.logging.RuntimeLog()
+                        keeper= BestIterKeeper(H, np.prod(size), baseline = 1e-8,
+                            #filename=pathlib.Path("best_state.nk"),
+                        )
+                        gs.run(n_iter=iterations, out=log, callback= [keeper.update])
+                        time_out = time.time()
+                        time_exe= time_out - time_in
+                        
+                        if exact_diag:
+                            E_ED, x_ED = Runner(oxa.cm).exact_energy_lanczos(eigenstates=True)
+                            E_ED = float(E_ED.squeeze(-1))
+                            keeper.E_ED = E_ED
 
 
-                    dump_callback(log, dump_setup)
-
-                
-
-                # Extract some results
-                vstate = keeper.best_state
-                E_best = np.array(keeper.best_energy)
-                vscore = np.array(keeper.vscore)
-                if ED:
-                    error=np.array(np.abs(E_best-E_ED)/np.abs(E_ED))
-                    results = np.array([E_best, E_ED, error, vscore])
-                else:
-                    results = np.array([E_best, vscore])
-                # Save the results
-
-                dump_setup ={
-                            'size': settings['size'], 
-                            'strength': strength,
-                            'theta': theta,
-                            'phi': phi, 
+                        sim_label = f"_{d}_{a}_{opt_name}_{learning_rate}"
+                        if dump_simulation:
+                            # For plotting architecture
+                            set_dim=[f"D({dim})" for dim in dimensions] 
+                            set_act=[f"A({act})" for act in activation_name]
+                            setup='||'
+                            for i in range(len(dimensions)):
+                                setup+=f" {set_dim[i]} |"
+                                setup+=f" {set_act[i]} |" if '0' not in set_act[i] else ''
+                                
+                            setup+="|"
                             
-                            'model': {
+                            dump_setup ={'size': size, 'theta': theta,
+                                        'phi': phi, 'opt_name': opt_name,
+                                        'learning_rate': learning_rate,
+                                        'time_exe': time_exe, 'architecture': setup,
+                                        'sim_label': sim_label
+                                    }
+                            dump_callback(log, dump_setup)
+
+                        
+
+                        # Extract some results
+                        vstate = keeper.best_state
+                        E_best = float(keeper.best_energy)
+                        vscore = float(keeper.vscore)
+
+                        if exact_diag:
+                            error=float(np.abs(E_best-E_ED)/np.abs(E_ED))
+                        else:
+                            E_ED = None
+                            x_ED = None
+                            error = None    
+
+                        # Save the results
+
+                        dump_setup ={
+
+                            'lattice':{
+                                'name': 'Triangular',
+                                'size': size, 
+                                'bc': kwargs_lattice['bc'],
+                            },
+                            'coupling_model': {
+                                'strength': strength,
+                                'theta': theta,
+                                'phi': phi, 
+                            },
+
+                            'model_NN': {
                                 'name': 'MLP',
                                 'dense_dim': str(dimensions),
                                 'activation': str(activation_name),
                                 
                             },
                             'sampler': {
-                                'name': settings['sampler']['type'],
-                                'n_samples': settings['sampler']['n_samples'], 
-                                'rng': str(vstate.sampler_state.rng )      
-                                        },
+                                'name': sampler_name,
+                                'n_samples': n_samples, 
+                                'rng': vstate.sampler_state.rng.tolist() 
+
+                            },
                             'optimizer': opt_name,
                             'learning_rate': learning_rate,
-                            'time_exe': time_exe, 
 
+                            'results':{
+                                'E_best': E_best,
+                                'E_ED': E_ED,
+                                'error': error,
+                                'vscore': vscore,
+                                'time_exe': time_exe, 
+
+                            }  
                         }
 
 
-                save_results(results, vstate, )
-                
+                        save_results(
+                            vstate, 
+                            dump_setup, 
+                            x_ED = x_ED, 
+                            write_folder = write_folder,
+                            sim_label = sim_label
+                        )
+                        
--- NN_utils.py
+++ NN_utils.py
@@ -1,5 +1,98 @@
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
+import netket as nk
+import json
+import flax
+import flax.linen as nn
+import jax
+from flax.serialization import to_bytes
+import copy
+import numpy.typing as npt
+from typing import Optional
+import pathlib
+from datetime import date
+from platform import architecture, python_version
+import os
+
+activation_dict={
+    'sigmoid': nn.sigmoid,
+    'tanh': nn.tanh,
+    'softmax': nn.softmax,
+    'gelu': nn.gelu,
+    'swish': nn.swish,
+    'selu': nn.selu,
+    'elu': nn.elu,
+    'softplus': nn.softplus,
+    'relu': nn.relu
+}
+
+optimizer_name_dict={
+    'Sgd': nk.optimizer.Sgd,
+    'adam': nk.optimizer.Adam,
+    'AdaGrad': nk.optimizer.AdaGrad
+}
+
+sampler_dict={
+    'MetropolisLocal': nk.sampler.MetropolisLocal,
+    'MetropolisExchange': nk.sampler.MetropolisExchange,
+    'MetropolisHamiltonian': nk.sampler.MetropolisHamiltonian
+}
+
+
+class BestIterKeeper:
+    """Store the values of a bunch of quantities from the best iteration.
+
+    "Best" is defined in the sense of lowest energy.
+
+    Args:
+        Hamiltonian: An array containing the Hamiltonian matrix.
+        N: The number of spins in the chain.
+        baseline: A lower bound for the V score. If the V score of the best
+            iteration falls under this threshold, the process will be stopped
+            early.
+        filename: Either None or a file to write the best state to.
+    """
+
+    def __init__(
+        self,
+        Hamiltonian: npt.ArrayLike,
+        N: int,
+        baseline: float,
+        filename: Optional[pathlib.Path] = None,
+    ):
+        self.Hamiltonian = Hamiltonian
+        self.N = N
+        self.baseline = baseline
+        self.filename = filename
+        self.vscore = np.inf
+        self.best_energy = np.inf
+        self.best_state = None
+
+    def update(self, step, log_data, driver):
+        """Update the stored quantities if necessary.
+
+        This function is intended to act as a callback for NetKet. Please refer
+        to its API documentation for a detailed explanation.
+        """
+        vstate = driver.state
+        energystep = np.real(vstate.expect(self.Hamiltonian).mean)
+        var = np.real(getattr(log_data[driver._loss_name], "variance"))
+        mean = np.real(getattr(log_data[driver._loss_name], "mean"))
+        varstep = self.N * var / mean**2
+
+        if self.best_energy > energystep:
+            self.best_energy = energystep
+            self.best_state = copy.copy(driver.state)
+            self.best_state.parameters = flax.core.copy(driver.state.parameters)
+            self.vscore = varstep
+
+            if self.filename != None:
+                with open(self.filename, "wb") as file:
+                    file.write(flax.serialization.to_bytes(driver.state))
+
+        return self.vscore > self.baseline
+
 
 def dump_callback(logger, settings, plot =  True, write = False):
 
@@ -77,17 +170,73 @@ def dump_callback(logger, settings, plot =  True, write = False):
                 np.array(vscore))
         
 
-def save_results(results, vstate, setup, x_ED = None):
-    size = setup['size']
-    N = int(np.prod(setup['size']))
-    theta = setup['theta']
-    phi = setup['phi']
-    write_folder = setup['write_folder']
-    architecture = setup['architecture']
-    opt_name = setup['optimizer']
-    learning_rate = setup['learning_rate']
+def save_results(vstate, setup, x_ED = None, write_folder = './', sim_label = ''):
+    """Save the results of the simulation.
+    Args:
+        vstate: The variational state.
+        setup: The setup dictionary containing the simulation parameters.
+        x_ED: The exact diagonalization results (optional).
+        write_folder: The folder to save the results.
+        sim_label: A label for the simulation.
+    """
 
+    os.makedirs(write_folder, exist_ok = True)
+
+    size = setup['lattice']['size']
+    strength = setup['coupling_model']['strength']
+    theta = setup['coupling_model']['theta']
+    phi = setup['coupling_model']['phi']
     
+    # Save the variational state
+    file = f"{size[0]}x{size[1]}_strength_{strength}_theta_{theta}_phi_{phi}_{sim_label}"
+    path_vstate = write_folder + "Oxalate_vstate_params_" + file + ".npz"
 
-    np.savetxt(write_folder + f"Oxalate_{size[0]}x{size[1]}_theta_{theta}_phi_{phi}_{sim_label}_results.txt",
-            results)
+    with open(path_vstate, "wb") as f:
+        f.write(to_bytes(vstate.parameters))
+
+
+    #Save simulation results, data and metadata
+    metadata = {
+        "date": date.today().strftime("%x"),
+        "architecture": architecture(),
+        "versions": {
+            "python": python_version(),
+            "numpy": np.__version__,
+            "scipy": sp.__version__,
+            "netket": nk.__version__,
+            "jax": jax.__version__,
+            "flax": flax.__version__
+        },
+        "device": str(jax.devices()[0])
+    }
+
+    setup['metadata'] = metadata
+    setup['results']['vstate'] = path_vstate
+
+    setup_path = write_folder + "Oxalate_results_" + file + ".json"
+    with open(setup_path, "w") as outfile:
+        json.dump(setup, outfile, separators=(",", ":"), sort_keys=True, indent=4)
+    
+    # Save exact diagonalization eigenstate 
+    if x_ED is not None: 
+        path_ED = write_folder + "Oxalate_ED_" + file + ".txt"
+        np.savetxt(path_ED, x_ED)
+
+def load_vstate(setup):
+
+    sampler_name = setup['']['']
+    model = setup
+
+    vs = nk.vqs.MCState(sampler, model, n_samples=..., seed=...)
+
+    vs_path = setup['results']['vstate']
+
+    with open(vs_path, "rb") as f:
+        vs.parameters = from_bytes(vs.parameters, f.read())
+
+
+
+
+        
+
+
