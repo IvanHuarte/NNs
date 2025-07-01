bf693ba97627a4551c7781954dd843c01c36a617
IvanHuarte <ivanhuarte@hotmail.com>
2025-07-01 10:57:58 +0200

    Updating MLP_simulation.py to new config structure

--- MLP_simulation.py
+++ MLP_simulation.py
@@ -4,47 +4,68 @@ import jax
 import jax.numpy as jnp
 import netket as nk
 import json
+import optax
 import time
+
+# Añadir los directorios necesarios
 import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent / "ATMOS_VA/VA_project/src"))
+
 
 from VA_project.model.model import OxalateJKGamma
 from VA_project.engine.runners import Runner
 from NN_module.NN_utils import (
-    dump_callback, save_results, BestIterKeeper,
-    activation_dict, sampler_dict, optimizer_name_dict
+    activation_dict, sampler_dict, scheduler_initializer, 
+    phase_stats_ED, phase_stats_vstate
 )
-from NN_module.models.MLP import (
-    MultiLayerPerceptron, MultiLayerPerceptron_Z2_Traslation,
-    MultiLayerPerceptron_Traslation
-    )
+from NN_module.sim_utils import (
+    save_results, dump_callback, BestIterKeeper
+)
+from NN_module.models.MLP import BatchedMultiLayerPerceptron
 
 jax.config.update("jax_enable_x64", True)
 jax.config.update("jax_platform_name", "gpu")
 jax.devices()
 
-with open("config.json",'r') as f:
+with open("/home/ihuarte/Escritorio/Ivan/NNs/config.json",'r') as f:
     config = json.load(f)
 
-strength = config['strength']          # Lattice and coupling model
+sizes = config['sizes']
 theta_list = config['theta_list']
 phi_list = config['phi_list']
-sizes = config['sizes']
 kwargs_lattice = config['kwargs_lattice']
 
 alpha_list = config['alpha_list']         # MLP architecture settings
 activation_list = config['activation_list']
-opt_name_list = config['opt_name_list']
-learning_rate_list = config['learning_rate_list']
+symm_2D = config['symm_2D']
+symm_Z2 = config['symm_Z2']
+trivial_Z2 = config['trivial_Z2']
+
+print(f"symm_2D: {symm_2D}")
+print(f"symm_Z2: {symm_Z2}")
+print(f"trivial_Z2: {trivial_Z2}\n \n")
 
 sampler_name = config['sampler']['name']            # Sampler settings
 n_samples = config['sampler']['n_samples']
 
-iterations = config['iterations']                   # Simulation settings
+epochs = config['lr_schedule']['epochs']                   # Simulation settings
+schedule = config['lr_schedule']
 exact_diag = config['exact_diagonalization']
 dump_simulation = config['dump_sim_callback']
 write = config['write_folder_sim']
 
-diag_shift= 0.001       # Diagonal shift for the SR preconditioner
+### Training schedule ###
+
+lr_schedule = scheduler_initializer(schedule['name'], schedule)
+optimizer = nk.optimizer.Sgd(learning_rate=lr_schedule)
+ds_schedule = optax.linear_schedule(1e-2, 1e-4, epochs)  # Diagonal shift for the SR preconditioner
+SR = nk.optimizer.SR(diag_shift=ds_schedule, holomorphic = True)
+
+alpha_list = [tuple(dim) for dim in alpha_list]
+activation_list = [[tuple(act) for act in activation] for activation in activation_list]
+
+rng = jax.random.PRNGKey(666)
 
 alpha_list = [tuple(dim) for dim in alpha_list]
 activation_list = [[tuple(act) for act in activation] for activation in activation_list]
