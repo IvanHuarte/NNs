bfe0b516c4e95852b205d1375c6d99ec4aebbaf4
IvanHuarte <ivanhuarte@hotmail.com>
2025-08-12 21:06:15 +0200

    CvT update

--- NN_module/models/CvT.py
+++ NN_module/models/CvT.py
@@ -36,30 +36,64 @@ class TriangularMaskedConv(nn.Module):
             dimension_numbers=("NHWC", "HWIO", "NHWC"),
         )
     
+class DepthPointwiseConv(nn.Module):
+    """
+    Depthwise pointwise convolution
+    """
+    channels: int
+    kernel: Tuple = (3, 3)
+    strides: Tuple = (1, 1)
+
+    @nn.compact
+    def __call__(self, x: jt.ArrayLike) -> jt.ArrayLike:
+        
+        #Depth-wise convolution
+        Ch_in = x.shape[-1]
+        x = nn.Conv(
+            features=Ch_in,               
+            kernel_size=self.kernel,      
+            feature_group_count=Ch_in,    
+            strides=self.strides,         
+            padding='SAME',                   # 'CIRCULAR' para BC periódicas
+            use_bias=False
+        )
+        # Normalizacion
+        x = nn.LayerNorm()(x)
+
+        # Point-wise convolution
+        x = nn.Conv(
+            features=self.channels,   
+            kernel_size=(1, 1),           
+            strides=(1, 1),                
+            padding='SAME',                 # 'CIRCULAR' para BC periódicas
+            use_bias=False
+        )(x)
+
+        return x
+    
 class CvTBlock(nn.Module):
 
     channels: int
-    kernel: Tuple =(3,3)
-    strides_qkv: Tuple = ((1,1),(1,1),(1,1))
-
-
+    kernel: Tuple = (3,3)
+    strides_qkv: Tuple[Tuple, Tuple, Tuple] = ((1,1),(1,1),(1,1))
 
     def __call__(self, x: jt.ArrayLike) -> jt.ArrayLike:
 
         # Convolutional projection
         # x (B,H,W,Ch)
+        B, H, W, Ch_in = x.shape
         res1 = x
         x = nn.LayerNorm()(x)
-        Q = TriangularMaskedConv(self.channels, self.strides[0])
-        K = TriangularMaskedConv(self.channels, self.strides[1])
-        V = TriangularMaskedConv(self.channels, self.strides[2])
-        size = (Q.shape[1], Q.shape[2])
+        Q = DepthPointwiseConv(self.channels, kernel=self.kernel, strides=self.strides_qkv[0])(x).reshape(B, -1, self.channels)
+        K = DepthPointwiseConv(self.channels, kernel=self.kernel, strides=self.strides_qkv[1])(x).reshape(B, -1, self.channels)
+        V = DepthPointwiseConv(self.channels, kernel=self.kernel, strides=self.strides_qkv[2])(x).reshape(B, -1, self.channels)
+        
 
         # Self-attention block
-        attn_scores = jnp.einsum('bqc,bkc->bqk', Q, K) / jnp.sqrt(self.channels)
+        attn_scores = jnp.matmul(Q,jnp.swapaxes(K,-2,-1)) / jnp.sqrt(self.channels)
         attn_weights = nn.softmax(attn_scores, axis=-1)
-        attn_out = jnp.einsum('bqk,bkc->bqc', attn_weights, V)
-        attn_out = attn_out.reshape(-1, *size, self.channels)
+        attn_scores = jnp.matmul(attn_weights, V)
+        attn_out = attn_out.reshape(B, H, W, self.channels)
 
         x = res1 + attn_out  # Residual
 
@@ -83,8 +117,6 @@ class StageBlock(nn.Module):
         Ch: Number of channels 
     """
     lattice_size = Tuple
-
-
 
     @nn.compact
     def __call__(self, x: jt.ArrayLike) -> jt.ArrayLike:
