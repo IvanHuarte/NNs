66f41f253dd91b02874f5eaba9a71bc0b4c8d238
IvanHuarte <ivanhuarte@hotmail.com>
2025-09-23 20:04:05 +0200

    Update

--- SplitTraining_J1J2.py
+++ SplitTraining_J1J2.py
@@ -261,8 +261,7 @@ for i, size in enumerate(sizes):
                         mean, std, psi = phase_stats_vstate(vstate)
                         print(f"VS phase: {mean} \u00b1 {std}  ({psi})")
 
-                        grads = vstate.gradient()
-                        print(grads)
+                        
 
             else:  # Training modulus and phase at the same time
 
