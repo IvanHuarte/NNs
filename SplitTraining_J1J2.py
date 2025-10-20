8eaa5809b56a08ed7683c300e50b21a3030cb737
IvanHuarte <ivanhuarte@hotmail.com>
2025-10-20 10:42:44 +0200

    Sign bias in CNNClsf

--- SplitTraining_J1J2.py
+++ SplitTraining_J1J2.py
@@ -229,7 +229,7 @@ for i, size in enumerate(sizes):
 
                     for epochs, mode in zip(segment, seg_modes):
 
-                        P0 = vstate.parameters
+                        # P0 = vstate.parameters
 
                         if mode == "M":
                             mode = "modulus"
@@ -285,8 +285,8 @@ for i, size in enumerate(sizes):
                         mean, std, psi = phase_stats_vstate(vstate)
                         print(f"VS phase: {mean} \u00b1 {std}  ({psi})")
 
-                        P1 = vstate.parameters
-                        print(compare_params(P0, P1))
+                        # P1 = vstate.parameters
+                        # print(compare_params(P0, P1))
                         # check_zero_grads(vstate, mask)
 
             else:  # Training modulus and phase at the same time
