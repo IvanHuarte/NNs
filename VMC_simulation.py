baf7c544eb3137f29efd89e0d131814ae1955e11
IvanHuarte <ivanhuarte@hotmail.com>
2025-11-28 14:06:11 +0100

    Update

--- VMC_simulation.py
+++ VMC_simulation.py
@@ -214,7 +214,7 @@ for i, size in enumerate(sizes):
             # Callbacks
             if enable_keeper:
                 keeper = BestIterKeeper(
-                    total_epochs, H, N, baseline=1e-8, mode="always"
+                    total_epochs, H, N, baseline=1e-8, mode="best_energy"
                 )
                 callbacks.append(keeper.update)
             # keeper.filename = 'Somewhere' #It allows you to store the parameters of the model for the state with lowest energy found.
@@ -372,7 +372,7 @@ for i, size in enumerate(sizes):
         results, mp_array_vs, mp_array_ED = measureNdump(keeper, time_exe, exact_diag)
 
         sim_config["SIM"]["sampler"]["nsamples"] = n_samples
-        sim_config["SIM"]["sampler"]["rng"] = list(vstate.sampler_state.rng)
+        sim_config["SIM"]["sampler"]["rng"] = jax.random.key_data(vstate.sampler_state.rng)
 
         ## Save the results
         dump_setup = {
