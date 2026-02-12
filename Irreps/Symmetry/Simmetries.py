e3b9f8f52fa71de9b6ebe5e3b46ce379ee192fb9
IvanHuarte <ivanhuarte@hotmail.com>
2026-02-12 20:32:57 +0100

    Update

--- Irreps/Symmetry/Simmetries.py
+++ Irreps/Symmetry/Simmetries.py
@@ -32,3 +32,113 @@ class Traslation:
         configuration = configuration.reshape(self.lattice_size)
         configuration = jnp.roll(configuration, 1, axis=self.axis).reshape(-1)
         return configuration
+
+
+
+
+def c4_operation(lattice_size):
+    def _operation(configuration):
+        configuration = configuration.reshape(lattice_size).T[::-1]
+        return configuration.reshape(-1)
+    return _operation
+
+
+
+class Cn():
+
+    def __init__(self, lattice_size, n):
+        """
+        C_n rotation symmetry
+        """
+
+        self.lattice_size = lattice_size
+
+        self.dim = 1
+        self.N = 4
+        self.character = lambda q, m: jnp.exp(2 * jnp.pi * q * m / n)
+
+        assert self.N != 1, f"Group size too low. N = 1"
+        assert all(L != 1 for L in self.lattice_size)
+
+        if n == 4:
+            self.operation = c4_operation(lattice_size)
+        
+
+    
+class Refl_H():
+    
+    def __init__(self, lattice_size):
+        """
+        Reflection symmetry respect to the horizontal axis
+        """
+
+        self.lattice_size = lattice_size
+
+        self.dim = 1
+        self.N = 2
+        self.character = lambda k, m: 1 if k==0 else (-1)**m
+
+        assert self.N != 1, f"Group size too low. N = 1"
+        assert all(L != 1 for L in self.lattice_size)
+
+    def operation(self, configuration):
+        return configuration.reshape(self.lattice_size)[::-1]
+    
+class Refl_V():
+    
+    def __init__(self, lattice_size):
+        """
+        Reflection symmetry respect to the horizontal axis
+        """
+
+        self.lattice_size = lattice_size
+
+        self.dim = 1
+        self.N = 2
+        self.character = lambda k, m: 1 if k==0 else (-1)**m
+
+        assert self.N != 1, f"Group size too low. N = 1"
+        assert all(L != 1 for L in self.lattice_size)
+
+    def operation(self, configuration):
+        return configuration.reshape(self.lattice_size)[:, ::-1]
+
+class Refl_D():
+    
+    def __init__(self, lattice_size):
+        """
+        Reflection symmetry respect to the horizontal axis
+        """
+
+        self.lattice_size = lattice_size
+
+        self.dim = 1
+        self.N = 2
+        self.character = lambda k, m: 1 if k==0 else (-1)**m
+
+
+        assert self.N != 1, f"Group size too low. N = 1"
+        assert all(L != 1 for L in self.lattice_size)
+
+    def operation(self, configuration):
+        return configuration.reshape(self.lattice_size).T
+    
+class Refl_AD():
+    
+    def __init__(self, lattice_size):
+        """
+        Reflection symmetry respect to the horizontal axis
+        """
+
+        self.lattice_size = lattice_size
+
+        self.dim = 1
+        self.N = 2
+        self.character = lambda k, m: 1 if k==0 else (-1)**m
+
+        assert self.N != 1, f"Group size too low. N = 1"
+        assert all(L != 1 for L in self.lattice_size)
+
+    def operation(self, configuration):
+        return configuration.reshape(self.lattice_size)[:, ::-1].T[:, ::-1]
+    
--- Irreps/Symmetry/__init__.py
+++ Irreps/Symmetry/__init__.py
@@ -1,15 +1,25 @@
-from .Simmetries import Z2, Traslation
+from .Simmetries import Z2, Traslation, Cn, Refl_H, Refl_V, Refl_D, Refl_AD
 
 SIMMETRY = {
     'Z2':Z2,
-    'Traslation': Traslation
+    'Traslation': Traslation,
+    "Cn": Cn,
+    "Refl_H": Refl_H,
+    "Refl_V": Refl_V,
+    "Refl_D": Refl_D,
+    "Refl_AD": Refl_AD
 }
 
 # EXTERNAL ARGS
 
 LATTICE_SIZE = {
     'lattice_size':[
-        'Traslation'
+        'Traslation',
+        'Cn',
+        "Refl_H",
+        "Refl_V",
+        "Refl_D",
+        "Refl_AD"
     ]
 }
 
