77e0352d8e868cff98f74ea9e586261efdebe910
IvanHuarte <ivanhuarte@hotmail.com>
2026-05-26 12:11:56 +0200

    Update

--- NN_module/callback/BestIterKeeper.py
+++ NN_module/callback/BestIterKeeper.py
@@ -71,7 +71,7 @@ class BestIterKeeper:
         vscore_step = self.N * var / mean**2
 
         if self.step > self.step_threshold or self.best_state is None:
-            if (self.best_state_energy > energy_step and vscore_step < 0.1) or self.best_state is None:
+            if (self.best_state_energy > energy_step and vscore_step < 0.05) or self.best_state is None:
                 self.best_state = copy.copy(vstate)
                 self.best_state_energy = energy_step
                 self.best_state_vscore = vscore_step
